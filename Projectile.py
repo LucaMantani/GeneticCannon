16f042bf65ccd7584965d4aeb571483a9f959bb7
Luca Mantani <luca@MacBook-Pro-di-Luca.local>
2018-09-08 16:09:49 +0200

    Implementation class Projectile

---
+++ Projectile.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class Projectile(object):
+
+    def __init__(self, mass):
+        self._mass = mass
+        self.pos = np.array([0.0, 0.0])
+        self.speed = np.array([0.0, 0.0])
+
+    @property
+    def mass(self):
+        return self._mass
