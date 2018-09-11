0796572b2635de484a6970b457feda6a1bfb3fb0
Luca Mantani <luca@MacBook-Pro-di-Luca.local>
2018-09-11 08:31:44 +0200

    Implementation of the class Physics

---
+++ Physics.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+class Physics(object):
+    """
+    It defines the Physics of the problem. In particular
+    the user can set the strength of gravity and the viscosity of the air.
+    """
+
+    def __init__(self, gravity=0, viscosity=0):
+        self._gravity = gravity
+        self._viscosity = viscosity
+
+    def timestep(self, projectile):
+        """
+        Modifies the projectile speed and position according to
+        Newton's mechanics laws.
+        """
+        projectile.speed += -self._gravity * np.array([0, 1])
+        - self._viscosity / projectile.mass * projectile.speed
+
+        projectile.pos += projectile.speed
